d758d05b1226e829b60dfb088fe2bfe52d03c993
Pranjal Biyani <pranjalbiyani1996@gmail.com>
2019-02-22 00:48:44 +0530

    Update Basic Class and Instances.py

--- Basics/Basic Class and Instances.py
+++ Basics/Basic Class and Instances.py
@@ -1,21 +1,31 @@
-class Employee:
+class Friends:
 
-	def __init__(self,first,last,pay):
+	def __init__(self,first,last,pocket_money):
 		self.first = first
 		self.last = last
-		self.pay = pay
-		self.email = first+'_'+last+'@companyname.com'
+		self.pocket_money = pocket_money
+		self.email = first+'_'+last+'@Friends.com'
 
 	def fullname(self):
 		return '{} {}'.format(self.first,self.last)
 
 
 
-emp1 = Employee('Pranjal','Biyani',1000000)
-emp2 = Employee('Single','Vyas',1000000)
-emp3 = Employee('Lodu','Dey',1000000)
-emp4 = Employee('Notanki','Shukla',1000000)
-emp5 = Employee('Party_Kab_Dega','Singh',1000000)
+fr1 = Friends('Pranjal','Biyani',1000000)
+fr2 = Friends('Karan','Vyas',1000000)
+fr3 = Friends('Indradeep','Dey',1000000)
+fr4 = Friends('Rohit','Shukla',1000000)
+fr5 = Friends('Karni','Singh',1000000)
 
-for x in range(5):
-	print(Employee.fullname('emp'+'x'))
+print(fr1.fullname())
+print(fr2.fullname())
+print(fr3.fullname())
+print(fr4.fullname())
+print(fr5.fullname())
+
+print(fr1.email)
+print(fr2.email)
+print(fr3.email)
+print(fr4.email)
+print(fr5.email)
+
